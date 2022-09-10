03691c981dcc7199cf13358e4781da29d355e087
Joni Turunen <joni.turunen@triplan.fi>
2022-09-10 09:03:30 +0300

    task: Redefine conts variables

---
+++ cmd/gow/consts.go
@@ -0,0 +1,17 @@
+package main
+
+const mainFileContents string = `package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.Println("Im waiting to be implemented")
+} 
+`
+const unnamedProjectName string = "/unnamed-project"
+const binDir = "/bin/"
+const cmdDir = "/cmd/"
+const pkgDir = "/pkg/"
+const mainDirPostfix = "main/"
