5f17dc06bcb74fb59db883d27900ca35b8f52202
Joni Turunen <joni.turunen@triplan.fi>
2022-09-10 13:51:30 +0300

    task: Add contents of test template

--- cmd/gow/consts.go
+++ cmd/gow/consts.go
@@ -10,6 +10,26 @@ func main() {
 	log.Println("Im waiting to be implemented")
 } 
 `
+
+const mainTestFileContents string = `package main
+
+import (
+	"testing"
+)
+
+func Test_example(t *testing.T) {
+	t.Run("Constructed with one parameter", func(t *testing.T) {
+		got := "Assertion"
+		expected := "Expected result"
+
+		if got != expected {
+			t.Errorf("GOT: %q\nEXPECTED: %q\n", got, expected)
+		}
+	})
+}
+
+`
+
 const unnamedProjectName string = "/unnamed-project"
 const binDir = "/bin/"
 const cmdDir = "/cmd/"
